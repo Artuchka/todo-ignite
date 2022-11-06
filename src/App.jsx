581b209377e8c61ffe07cf7e0798cd9ac6b3fd67
JohnDoeee <yandex949@gmail.com>
2022-11-06 19:35:45 +0300

    presetup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./main.scss"
+
+function App() {
+	return (
+		<div className="App">
+			<header className="header">
+				<span>to</span>
+				<span>do</span>
+			</header>
+		</div>
+	)
+}
+
+export default App
